f561ccd09f91c5d1781d3695b6f9e7de5c36a1f5
MinTuan2124 <166462832+MinTuanit@users.noreply.github.com>
2025-11-02 22:17:18 +0700

    Update vnpay.js

--- src/config/vnpay.js
+++ src/config/vnpay.js
@@ -3,5 +3,5 @@ module.exports = {
   vnp_TmnCode: "PZDVMXFT",
   vnp_HashSecret: "1AWRSIE8Q8P5JPRNQBO35E3SVKOPTNVI",
   vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html", // URL cổng sandbox
-  vnp_ReturnUrl: "localhost:5173/vnpay-result", // URL callback khi thanh toán xong
+  vnp_ReturnUrl: "http://localhost:5173/vnpay-result", // URL callback khi thanh toán xong
 };
